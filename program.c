39c0c2c77202a8375f2593b2e21e19d605311cfa
AndresSal <andres.salazar01@epn.edu.ec>
2017-05-10 21:40:07 -0500

    manejo de archivo chemicals.tsv

--- program.c
+++ program.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(int argc, char** argv)
 {
 
@@ -7,24 +7,42 @@ int M[100][3];
 int i,j;
 FILE *entrada;
 
-char id[100];
-char formula[100];
-
+char id[20];
+char formula[20];
+char intro[100];
+char *token;
+char s[2]="\n";
 //lectura del archivo
 
 entrada = fopen("chemicals.tsv","r");
 
 if (entrada == NULL)
 {
+	printf("Archivo vacio\n");
 	return 1;
 }
 
 printf("\nEl contenido del archivo es \n\n");
-while(feof(entrada) == 0)
-{
-	fgets(formula,100,entrada);
-	printf("%s",formula);
-}
+	
+	while(feof(entrada)==0)
+	{
+		fgets(intro,30,entrada);
+		puts(intro);
+	}
+
+	
+
+	
+	token = strtok(intro, s);
+	
+	while(token != NULL)
+	{
+		printf("%s\n",token);
+
+		token = strtok(NULL,s);
+	}	
+
+
 fclose(entrada);
 
 //
